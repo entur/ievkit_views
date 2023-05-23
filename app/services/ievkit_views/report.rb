15d0f0aba460890a0858c76a698bb00a6c8278a2
Mansoor Sajjad <mansoor.sajjad@entur.org>
2023-05-23 11:02:41 +0200

    Adding double splat operator for getting the options hash.

--- app/services/ievkit_views/report.rb
+++ app/services/ievkit_views/report.rb
@@ -101,7 +101,7 @@ module IevkitViews
         end
         error[:test_name] = I18n.t("compliance_check_results.details.#{error[:test_id]}")
         begin
-            error[:error_name] = I18n.t("compliance_check_results.details.detail_#{error[:error_id]}", error)
+            error[:error_name] = I18n.t("compliance_check_results.details.detail_#{error[:error_id]}", **error)
         rescue => e
           Ievkit::Log.logger.error e.message
           error[:error_name] = e.message
